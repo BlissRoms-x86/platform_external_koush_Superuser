67221ef8560cae24d15c371acaffd992a6672d2c
Jon West <electrikjesus@gmail.com>
2019-04-11 19:11:22 -0400

    revert a couple changes

--- Superuser/src/com/koushikdutta/superuser/PolicyFragmentInternal.java
+++ Superuser/src/com/koushikdutta/superuser/PolicyFragmentInternal.java
@@ -168,7 +168,7 @@ public class PolicyFragmentInternal extends ListContentFragmentInternal {
                 switch (which) {
                 case 0:
                     if(permissionChange.equalsIgnoreCase(
-                     getResources().getText(R.string.su_allow).toString())){
+                     getResources().getText(R.string.allow).toString())){
                  up.setPolicy(UidPolicy.ALLOW);
                     }
                     else{
