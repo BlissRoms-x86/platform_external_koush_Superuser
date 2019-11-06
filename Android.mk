34953d4e7c794180307552e8a933b114b7014ae7
Unknown <electrikjesus@gmail.com>
2019-11-05 20:20:25 -0500

    Update for Android-x86

--- Android.mk
+++ Android.mk
@@ -3,6 +3,16 @@
 
 my_path := $(call my-dir)
 
+ifdef SUPERUSER_EMBEDDED
+SUPERUSER_PACKAGE := com.android.settings
+else
+ifeq ($(SUPERUSER_PACKAGE),)
+SUPERUSER_PACKAGE := com.thirdparty.superuser
+endif
+SUPERUSER_PACKAGE := com.thirdparty.superuser
+include $(my_path)/Superuser/Android.mk
+endif
+
 LOCAL_PATH := $(my_path)
 include $(CLEAR_VARS)
 
@@ -12,7 +22,8 @@ LOCAL_FORCE_STATIC_EXECUTABLE := true
 LOCAL_STATIC_LIBRARIES := libc libcutils libselinux
 LOCAL_C_INCLUDES := external/sqlite/dist
 LOCAL_SRC_FILES := Superuser/jni/su/su.c Superuser/jni/su/daemon.c Superuser/jni/su/activity.c Superuser/jni/su/db.c Superuser/jni/su/utils.c Superuser/jni/su/pts.c Superuser/jni/sqlite3/sqlite3.c
-LOCAL_CFLAGS := -DSQLITE_OMIT_LOAD_EXTENSION -DREQUESTOR=\"$(SUPERUSER_PACKAGE)\"
+LOCAL_CFLAGS := -DSQLITE_OMIT_LOAD_EXTENSION -DREQUESTOR=\"$(SUPERUSER_PACKAGE)\" \
+	-Wno-unused-variable -Wno-unused-parameter 
 
 ifdef SUPERUSER_PACKAGE_PREFIX
   LOCAL_CFLAGS += -DREQUESTOR_PREFIX=\"$(SUPERUSER_PACKAGE_PREFIX)\"
@@ -25,9 +36,28 @@ endif
 LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT_SBIN)
 include $(BUILD_EXECUTABLE)
 
+SYMLINKS := $(addprefix $(TARGET_OUT)/bin/,su)
+$(SYMLINKS):
+	@echo "Symlink: $@ -> /system/xbin/su"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf ../xbin/su $@
+
+# We need this so that the installed files could be picked up based on the
+# local module name
+ALL_MODULES.$(LOCAL_MODULE).INSTALLED := \
+    $(ALL_MODULES.$(LOCAL_MODULE).INSTALLED) $(SYMLINKS)
+
 include $(CLEAR_VARS)
 
 ifdef SUPERUSER_EMBEDDED
+
+# make sure init.superuser.rc is imported from
+# init.rc or similar
+
+SUPERUSER_RC := $(TARGET_ROOT_OUT)/init.superuser.rc
+$(SUPERUSER_RC): $(LOCAL_PATH)/init.superuser.rc | $(ACP)
+	$(copy-file-to-new-target)
 
 SUPERUSER_MARKER := $(TARGET_OUT_ETC)/.has_su_daemon
 $(SUPERUSER_MARKER): $(LOCAL_INSTALLED_MODULE)
